d4e3c5c4948df10ce4d5627d25629890af18bce7
Katrina Lui <katrinawlui@gmail.com>
2017-08-03 01:46:25 -0700

    Create Map class

--- W1D5/abstract_data_types.rb
+++ W1D5/abstract_data_types.rb
@@ -33,3 +33,32 @@ class Queue
     @queue.dup
   end
 end
+
+class Map
+  def initialize
+    @map = []
+  end
+
+  def assign(key, value)
+    pair_idx = @map.index { |pair| pair[0] == key }
+    pair_idx ? @map[pair_idx][1] = value : @map << [key, value]
+  end
+
+  def lookup(key)
+    @map.each { |pair| return pair[1] if pair[0] == key }
+  end
+
+  def remove(key)
+    @map.reject! { |pair| pair[0] == key }
+  end
+
+  def show
+    deep_dup(@map)
+  end
+
+  private
+
+  def deep_dup(array)
+    array.map { |el| el.is_a?(Array) ? deep_dup(el) : el }
+  end
+end
