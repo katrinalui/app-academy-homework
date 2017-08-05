7d0afa5cd536c76662bf849fe71a641f7f4312dd
Katrina Lui <katrinawlui@gmail.com>
2017-08-04 22:24:51 -0700

    Initial commit for Simon

---
+++ W2D1/lib/simon.rb
@@ -0,0 +1,54 @@
+class Simon
+  COLORS = %w(red blue green yellow)
+
+  attr_accessor :sequence_length, :game_over, :seq
+
+  def initialize
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
+  end
+
+  def play
+    until @game_over
+      take_turn
+      system("clear")
+    end
+
+    game_over_message
+    reset_game
+  end
+
+  def take_turn
+    show_sequence
+    require_sequence
+    round_success_message
+    @sequence_length += 1
+  end
+
+  def show_sequence
+    add_random_color
+  end
+
+  def require_sequence
+
+  end
+
+  def add_random_color
+    @seq << COLORS.sample
+  end
+
+  def round_success_message
+    puts "Good job!"
+  end
+
+  def game_over_message
+    puts "Game over!"
+  end
+
+  def reset_game
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
+  end
+end
