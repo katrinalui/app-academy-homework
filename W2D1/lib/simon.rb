011d838903a8de7a7d32175c18dbeb9e6b58746c
Katrina Lui <katrinawlui@gmail.com>
2017-08-06 19:31:31 -0700

    Add code to make game playable

--- W2D1/lib/simon.rb
+++ W2D1/lib/simon.rb
@@ -22,16 +22,24 @@ class Simon
   def take_turn
     show_sequence
     require_sequence
-    round_success_message
+    round_success_message unless @game_over
     @sequence_length += 1
   end
 
   def show_sequence
     add_random_color
+    @seq.each_with_index do |color, i|
+      puts "#{i + 1} - #{color}"
+      sleep(1)
+      system("clear")
+    end
   end
 
   def require_sequence
-
+    puts "What was the sequence?"
+    print ">"
+    guess = gets.chomp.split(" ")
+    @game_over = true if guess != @seq
   end
 
   def add_random_color
@@ -50,5 +58,18 @@ class Simon
     @sequence_length = 1
     @game_over = false
     @seq = []
+  end
+end
+
+if $PROGRAM_NAME == __FILE__
+  game = Simon.new
+  end_game = false
+  until end_game
+    system("clear")
+    game.play
+    puts "Do you give up? (y/n)"
+    print ">"
+    input = gets.chomp.downcase
+    end_game = true if input == "y"
   end
 end
