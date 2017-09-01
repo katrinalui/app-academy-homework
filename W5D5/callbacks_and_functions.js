b50acb64bbb84e67de244b720d1b0d07afd14b79
Katrina Lui <katrinawlui@gmail.com>
2017-08-31 23:47:33 -0700

    Complete intro to callbacks and functions

---
+++ W5D5/callbacks_and_functions.js
@@ -0,0 +1,32 @@
+// Run in Chrome
+// window.setTimeout(function () {
+//     alert('HAMMERTIME');
+// }, 5000);
+//
+// function hammerTime(time) {
+//   window.setTimeout(function () {
+//       alert(`${time} is hammertime!`);
+//   });
+// }
+
+// Run in Node
+const readline = require('readline');
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function teaAndBiscuits () {
+  reader.question("Would you like some tea? ", function (res) {
+    const first = (res === 'yes') ? "do" : "don\'t";
+    reader.question("Would you like some biscuits? ", function (res2) {
+      const second = (res2 === 'yes') ? "do" : "don\'t";
+
+      console.log(`So you ${first} want tea and you ${second} want biscuits.`);
+      reader.close();
+    });
+  });
+}
+
+teaAndBiscuits ();
